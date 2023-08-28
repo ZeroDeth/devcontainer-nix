5fb24fe5627848966de0ddba60b91398a6d1450c
Sherif Abdalla <sherif@abdalla.uk>
2023-08-28 20:00:46 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# devcontainer-nix
-VSCode devcontainer with Nix packages.
+# Devcontainer for Nix
+
+[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/ZeroDeth/devcontainer-nix)
+
+VSCode devcontainer for Nix
+
+This devcontainer contains Nix and hooks to install nix recipe as devcontainers.
+This is the base image that can be used to extend your own devcontainer based on nix.
+
+## Getting started
+
+The host machine must have:
+
+* VS Code
+  * https://code.visualstudio.com/download
+* WSL 2 (if running Windows)
+  * https://ripon-banik.medium.com/how-to-install-wsl2-offline-b470ab6eaf0e
+* DevContainers VS Code Extension
+  * Download from https://marketplace.visualstudio.com/items?itemName=ms-vscode-remote.remote-containers
+
+## Tasks
+
+### build
+
+```
+podman build --progress=plain -t devcontainer:latest .
+```
+
+### run
+
+```
+podman run --rm -it devcontainer:latest
+```
---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    xc = {
+      url = "github:joerdav/xc";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, xc }: {
+    packages."x86_64-linux".default = let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      in pkgs.buildEnv {
+        name = "packages";
+        paths = with pkgs; [
+          astyle # Code formatter for C.
+          azure-cli # Azure CLI.
+          azure-storage-azcopy # Azure CLI copy tool.
+          bat # Colourized cat.
+          cargo # Rust tooling.
+          ccls # C LSP Server.
+          cmake # C tooling.
+          d2 # Text-based diagramming tool.
+          delve # Go debugger.
+          docker # Container tooling.
+          entr # Execute command when files change.
+          fd # Find that respects .gitignore.
+          fuse-overlayfs # Filesystem for rooless podman.
+          fzf # Fuzzy search.
+          gcc # Compiler.
+          git # Source code management tool.
+          gitAndTools.gh # Github CLI.
+          gnupg # Tool for handling PGP encrypt/decrypt.
+          go # Programming language.
+          gopls # LSP for Go.
+          gotools # Additional Go tooling.
+          gradle # Build tool.
+          graphviz # Build SVGs of graphs (maths graphs).
+          htop # Fancy version of top.
+          imagemagick # Image processing.
+          jdk # Development.
+          jq # JSON query at the CLI.
+          jre # Runtime.
+          kubectl # Kubernetes management tool.
+          kubernetes-helm # Manage Kubernetes deployments.
+          llvm # Used by Raspberry Pi Pico SDK.
+          lua5_4 # Scripting language.
+          maven # Java build tooling.
+          minicom # Serial monitor.
+          neovim # Better version of Vim.
+          ninja # Used by Raspberry Pi Pico SDK, build tool.
+          nmap # Port scanner.
+          nodejs-18_x # Node LTS version.
+          podman # Docker alternative.
+          podman-compose # Alternative to docker-compose.
+          powerline # Status line UI fancy.
+          python310Packages.python-lsp-server
+          python311 # Python 3.11.
+          ranger # File manager.
+          ripgrep # Fast grep.
+          rust-analyzer # Rust language server.
+          rustc # Rust compiler.
+          rustfmt # Rust formatter.
+          shadow # Required to run podman containers in podman.
+          silver-searcher # Fast grep (ag).
+          sumneko-lua-language-server # LSP for Lua.
+          terraform # Infrastructure management language.
+          terraform-ls # Terraform Language Server.
+          tflint # Terraform linting tool.
+          tmux # Terminal multiplexer.
+          tree # Tree of file systems.
+          tunctl # Utility for controlling tuntap interfaces. Required to start podman containers with open ports.
+          unzip # Unzip files.
+          urlscan # Find URLs in your terminal
+          wget # curl, but it follows links automatically etc.
+          xc.packages."x86_64-linux".xc # Task manager.
+          yarn # Alternative to NPM for JavaScript.
+          zip # Zip files.
+        ];
+      };
+  };
+}
